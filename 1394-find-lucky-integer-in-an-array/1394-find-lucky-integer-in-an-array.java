4aa5ee8a4128836d8394b8f053a5dbdf47e07257
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-07-20 21:55:14 +0530

    07-20-2025 1394-find-lucky-integer-in-an-array Easy .java 5 ms (61.37%) 43.3 MB (74.45%)

--- 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.java
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.java
@@ -2,23 +2,36 @@ import java.util.Arrays;
 
 class Solution {
      int findLucky(int[] arr) {
+        int max=-1;
+    HashMap<Integer,Integer> hp=new HashMap();
+    for(int i=0;i<arr.length;i++){
+        hp.put(arr[i], hp.getOrDefault(arr[i],0)+1);
+    }
 
+ 
+      for(Map.Entry<Integer, Integer> entry : hp.entrySet()) {
+            //System.out.println(entry.getKey() + " " + entry.getValue() + " " + (entry.getKey() == entry.getValue()));
+            if(entry.getKey().equals(entry.getValue())) {
+                max = Math.max(max, entry.getKey());
+            }
+        }
+   return max;
       
-      int freq[]=new int[501];
+    //   int freq[]=new int[501];
 
 
 
-        for(int i=0;i<arr.length;i++){
+    //     for(int i=0;i<arr.length;i++){
           
-            freq[arr[i]]++;
-        }
+    //         freq[arr[i]]++;
+    //     }
 
-       for(int i=freq.length-1;i>0;i--){
-        if(freq[i]==i){
-            return i;
-        }
-       }
-       return -1;
+    //    for(int i=freq.length-1;i>0;i--){
+    //     if(freq[i]==i){
+    //         return i;
+    //     }
+    //    }
+    //    return -1;
       
         
     }
