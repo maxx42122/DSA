834d3cd50f3046ec07227495f55979b5817068a0
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-07-22 00:02:08 +0530

    07-22-2025 1534-count-good-triplets Easy .java 9 ms (96.71%) 41.3 MB (77.27%)

--- 1534-count-good-triplets/1534-count-good-triplets.java
+++ 1534-count-good-triplets/1534-count-good-triplets.java
@@ -6,11 +6,11 @@ class Solution {
         int n = arr.length;
 
         // Iterate through all possible indices i, j, k such that i < j < k
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
+        for (int i = 0; i <= n-3; i++) {
+            for (int j = i + 1; j <= n-2; j++) {
                 // Check the first condition. We can skip the inner loop if it fails.
                 if (Math.abs(arr[i] - arr[j]) <= a) {
-                    for (int k = j + 1; k < n; k++) {
+                    for (int k = j + 1; k <= n-1; k++) {
                         // Check the remaining two conditions
                         if (Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c) {
                             count++;
