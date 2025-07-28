c850e93db4a14113796c259eb706c7780a0648ae
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-07-28 12:16:18 +0530

    07-28-2025 0910-smallest-range-ii Medium .java 12 ms (68.29%) 45.5 MB (66.83%)

---
+++ 0910-smallest-range-ii/0910-smallest-range-ii.java
@@ -0,0 +1,23 @@
+
+class Solution {
+    public int smallestRangeII(int[] nums, int k) {
+   
+
+    // int maxx=Integer.MIN_VALUE;
+    //     int minn=Integer.MAX_VALUE;
+     Arrays.sort(nums);
+        int r=nums[nums.length-1]-nums[0];
+
+        for(int i=0;i<nums.length-1;i++){
+            int j=i+1;
+          int  maxx=Math.max(nums[nums.length-1]-k,nums[i]+k);
+            int minn=Math.min(nums[0]+k,nums[j]-k);
+             r=Math.min(r,(maxx-minn));
+        }
+      
+          
+      
+        return r; 
+    
+    }
+}
