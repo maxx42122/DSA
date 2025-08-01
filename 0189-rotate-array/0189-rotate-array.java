8cd5f19fe8484f4456b380f119881d1ab37c0f64
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-08-01 14:41:08 +0530

    08-01-2025 0189-rotate-array Medium .java 0 ms (100%) 57.5 MB (36.49%)

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,17 +1,55 @@
+// class Solution {
+//     public void rotate(int[] nums, int k) {
+//       int n=nums.length;
+//       k=k%n;
+//         int start=n-k;
+//         ArrayList<Integer>arr=new ArrayList<>();
+//         for(int i=start;i<n;i++){
+//             arr.add(nums[i]);
+//         }
+//         for(int i=0;i<start;i++){
+//             arr.add(nums[i]);
+//         }
+//     for (int i = 0; i < arr.size(); i++) {
+//     nums[i] = arr.get(i);
+// }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
     public void rotate(int[] nums, int k) {
       int n=nums.length;
-      k=k%n;
-        int start=n-k;
-        ArrayList<Integer>arr=new ArrayList<>();
-        for(int i=start;i<n;i++){
-            arr.add(nums[i]);
-        }
-        for(int i=0;i<start;i++){
-            arr.add(nums[i]);
-        }
-    for (int i = 0; i < arr.size(); i++) {
-    nums[i] = arr.get(i);
-}
+     k=k%n;
+        int start=k;
+
+        rev(nums,0,n-1);
+        rev(nums,0,start-1);
+        rev(nums,start,n-1);
+    }
+
+       private void rev(int nums[],int start,int end){
+            while(start<end){
+             int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start ++;
+        end --;
+            }
+       
     }
 }
