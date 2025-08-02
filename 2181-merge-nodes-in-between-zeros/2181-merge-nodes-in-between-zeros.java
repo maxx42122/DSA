139b0d518a3af27c8fbdaf17feaf2d8fc67310f0
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-08-02 12:34:56 +0530

    08-02-2025 2181-merge-nodes-in-between-zeros Medium .java 4 ms (99.56%) 79.7 MB (91.97%)

---
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeNodes(ListNode head) {
+
+     
+        ListNode node=head.next;
+        ListNode temp=node;
+    
+       
+        while(temp!=null){
+             int sum=0;
+          
+            while(temp.val!=0){
+                  sum+=temp.val;             
+                temp=temp.next;
+            }
+            node.val=sum;
+              temp=temp.next;
+              node.next=temp;
+              node=temp;
+
+        }
+        head=head.next;
+        return head;
+        
+    }
+}
