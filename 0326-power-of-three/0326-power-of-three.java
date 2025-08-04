b4753cfdc8cd97080021ce14184a08c9d3dd9995
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-08-05 00:39:08 +0530

    08-05-2025 0326-power-of-three Easy .java 8 ms (84.42%) 44.2 MB (39.6%)

---
+++ 0326-power-of-three/0326-power-of-three.java
@@ -0,0 +1,16 @@
+class Solution {
+     public boolean isPowerOfThree(int n) {
+  int dig=0;
+        while(n>0){
+               dig+=(n%3);
+               if(n==1 && dig==1)return true;
+            
+             
+                n/=3;
+
+        }
+     
+        return false;
+        
+    }
+}
