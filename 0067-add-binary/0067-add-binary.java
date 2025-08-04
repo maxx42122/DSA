0409fc97d82fc26335a1ff004cd0f39cbf6090ad
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-08-04 12:52:19 +0530

    08-04-2025 0067-add-binary Easy .java 1 ms (99.84%) 42 MB (91.56%)

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int bitA = (i >= 0) ? a.charAt(i--) - '0' : 0;
+            int bitB = (j >= 0) ? b.charAt(j--) - '0' : 0;
+
+            int sum = bitA + bitB + carry;
+            result.append(sum % 2); // current bit
+            carry = sum / 2;        // carry for next bit
+        }
+
+        return result.reverse().toString();
+    }
+}
