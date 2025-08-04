7d8515079a222b6105b3eddb166b703c47811468
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-08-04 11:08:38 +0530

    08-04-2025 0007-reverse-integer Medium .java 1 ms (81.63%) 41.2 MB (33.22%)

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,18 +1,15 @@
 class Solution {
     public int reverse(int x) {
-      
        long num=0;
         while(x!=0){
             int dig=x%10;
             num=num*10+dig;
             x=x/10;
-
         }
 
          if( num> Integer.MAX_VALUE || num< Integer.MIN_VALUE){
             return 0;
-        }
-       
+         }
         return(int) num;
         
     }
