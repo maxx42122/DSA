01fbcfd622652462956c28ccf8e5539f14a646ad
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-08-04 23:29:32 +0530

    08-04-2025 0292-nim-game Easy .java 0 ms (100%) 40.8 MB (16.82%)

---
+++ 0292-nim-game/0292-nim-game.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n==1 || n==2 || n==3)return true;
+        if(n%4==0)return false;
+
+        if(n%3==0 || n%3==1 ||n%3==2)return true;
+        return false;
+        
+    }
+}
