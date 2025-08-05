4305936fa4051fed8d52454549048fcd1dccf68e
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-08-06 01:16:51 +0530

    08-06-2025 0367-valid-perfect-square Easy .java 0 ms (100%) 40.5 MB (38.47%)

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,15 @@
+class Solution {
+   public boolean isPerfectSquare(int num) {
+    if (num < 2) return true;
+    long left = 2, right = num / 2;
+    while (left <= right) {
+        long mid = left + (right - left) / 2;
+        long guess = mid * mid;
+        if (guess == num) return true;
+        if (guess < num) left = mid + 1;
+        else right = mid - 1;
+    }
+    return false;
+}
+
+}
