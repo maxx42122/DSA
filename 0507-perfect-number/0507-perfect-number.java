1739a0ebcfc14a4b14105a573b7df755aa66f597
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-08-06 13:19:21 +0530

    08-06-2025 0507-perfect-number Easy .java 2105 ms (19.58%) 40 MB (98.13%)

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,19 +1,17 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        int x=getfact(num);
-
-        if(num==x)return true;
-        return false;
-        
-    }
-    private int getfact(int n){
         int sum=0;
 
-        for(int i=1;i<n/2+1;i++){
-            if(n%i==0){
+        for(int i=1;i<num/2+1;i++){
+            if(num%i==0){
                 sum+=i;
             }
         }
-        return sum;
+        
+
+        if(num==sum)return true;
+        return false;
+        
     }
+   
 }
