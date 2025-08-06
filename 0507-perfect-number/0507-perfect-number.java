f8ce741b7e1c5f8a242068259a5fbc665142df3a
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-08-06 13:18:51 +0530

    08-06-2025 0507-perfect-number Easy .java 2111 ms (12.46%) 40.7 MB (19.36%)

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -9,7 +9,7 @@ class Solution {
     private int getfact(int n){
         int sum=0;
 
-        for(int i=1;i<n;i++){
+        for(int i=1;i<n/2+1;i++){
             if(n%i==0){
                 sum+=i;
             }
