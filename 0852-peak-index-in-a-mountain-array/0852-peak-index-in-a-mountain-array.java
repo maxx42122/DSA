8412aaa252e0285f37da77bff3848ec696f3deca
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-08-10 19:43:35 +0530

    08-10-2025 0852-peak-index-in-a-mountain-array Medium .java 1 ms (14.99%) 56.3 MB (59.12%)

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,35 +1,36 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
 
-        int n=arr.length-1;
-        int end=n;
-        int start=0;
-        int max=0;
-        int res=0;
-          while(start<=end){
-           int  mid=start+(end-start)/2;
-      
+        // int end=arr.length-1;;
+        // int start=0;
+        // int max=0;
+        // int res=0;
+        //   while(start<=end){
+        //    int  mid=start+(end-start)/2;
 
-           if(max<arr[mid]){
-            max=Math.max(arr[mid],max);
-                 res=mid;
-
-
-           }
+        //    if(max<arr[mid]){
+        //     max=Math.max(arr[mid],max);
+        //          res=mid;
+        //    }
         
-            if(arr[mid]<=arr[mid+1]){
-                     start=mid+1;
-               
+        //     if(arr[mid]<=arr[mid+1]){
+        //              start=mid+1;  
              
-            }else {
-                  end=mid-1;
-              
+        //     }else {
+        //           end=mid-1;     
                
-            }
-           
+        //     }
 
+        // }
+int max=0;
+int j=0;
+        for(int i=0;i<arr.length;i++){
+            if(max<arr[i]){
+                max=arr[i];
+                j=i;
+            }
         }
-        return res;
+        return j;
         
     }
 }
