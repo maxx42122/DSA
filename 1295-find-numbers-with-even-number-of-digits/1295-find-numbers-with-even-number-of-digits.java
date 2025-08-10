7971c3fb6618d7df4cf4e93d0d96157e596dbf8b
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-08-10 14:56:00 +0530

    08-10-2025 1295-find-numbers-with-even-number-of-digits Easy .java 1 ms (98.83%) 43.1 MB (38.41%)

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.java
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.java
@@ -1,31 +1,39 @@
 class Solution {
-    public int findNumbers(int[] nums) {
-        int count=0;
+    // public int findNumbers(int[] nums) {
+    //     int count=0;
 
-        for(int i=0;i<nums.length;i++){
+    //     for(int i=0;i<nums.length;i++){
 
-            boolean res=count(nums[i]);
-            if(res)count++;
+    //         boolean res=count(nums[i]);
+    //         if(res)count++;
 
 
-        }
-        return count;
+    //     }
+    //     return count;
         
-    }
-    private boolean count(int num){
+    // }
+    // private boolean count(int num){
         
       
-            int cnt=0;
-            while(num>0){
-                int dig=num%10;
-                cnt++;
-                num/=10;
+    //         int cnt=0;
+    //         while(num>0){
+    //             int dig=num%10;
+    //             cnt++;
+    //             num/=10;
 
-            }
-            if(cnt%2==0)return true;
-            return false;
+    //         }
+    //         if(cnt%2==0)return true;
+    //         return false;
 
 
-        }
+    //     }
+
+     public int findNumbers(int[] nums) {
+        int count=0;
+        for(int n:nums)
+         if((n>9 && n<100) || (n>999 && n<10000) || (n==100000))
+          count++;
+        return count;
+    }
     
 }
