68115413ec99ad0b578d24393d30aac1cb06a88f
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-08-21 10:39:36 +0530

    08-21-2025 0081-search-in-rotated-sorted-array-ii Medium .java 0 ms (100%) 43.3 MB (67.21%)

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        for(int i:nums){
+            if(target==i){
+                return true;
+            }
+        }
+
+        return false;
+        
+    }
+}
