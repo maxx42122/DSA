63197b54927d3b3ecf9b1d770ec278ce6dd2c119
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-08-22 22:16:45 +0530

    08-22-2025 3195-find-the-minimum-area-to-cover-all-ones-i Medium .java 5 ms (94.31%) 197.1 MB (27.64%)

---
+++ 3195-find-the-minimum-area-to-cover-all-ones-i/3195-find-the-minimum-area-to-cover-all-ones-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minimumArea(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        int minRow = rows, maxRow = -1;
+        int minCol = cols, maxCol = -1;
+
+        // Traverse the grid to find the bounds of the rectangle
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                    minRow = Math.min(minRow, i);
+                    maxRow = Math.max(maxRow, i);
+                    minCol = Math.min(minCol, j);
+                    maxCol = Math.max(maxCol, j);
+                }
+            }
+        }
+
+        // Compute the area
+        return (maxRow - minRow + 1) * (maxCol - minCol + 1);
+    }
+}
