2fcba8e98a24343f17b802cccb989a8fdffaacdb
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-09-06 22:36:18 +0530

    09-06-2025 3516-find-closest-person Easy .java 1 ms (33.2%) 41.2 MB (20.41%)

--- 3516-find-closest-person/3516-find-closest-person.java
+++ 3516-find-closest-person/3516-find-closest-person.java
@@ -1,8 +1,15 @@
 class Solution {
   public int findClosest(int x, int y, int z) {
-    int a = Math.abs(x - z);
-    int b = Math.abs(y - z);
-    if (a == b) return 0;
-    return a < b ? 1 : 2;
+
+    int a=Math.abs(z-x);
+    int b=Math.abs(z-y);
+   
+    if(b>a){
+        return 1;
+    }
+    else if(a>b){
+        return 2;
+    }
+      return 0;
   }
 }
