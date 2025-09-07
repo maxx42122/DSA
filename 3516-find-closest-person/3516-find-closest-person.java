ee2f8ae1264c8d61ed93920c60385a60bda92e5a
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-09-07 11:54:12 +0530

    09-07-2025 3516-find-closest-person Easy .java 0 ms (100%) 40.8 MB (71.76%)

--- 3516-find-closest-person/3516-find-closest-person.java
+++ 3516-find-closest-person/3516-find-closest-person.java
@@ -4,12 +4,8 @@ class Solution {
     int a=Math.abs(z-x);
     int b=Math.abs(z-y);
    
-    if(b>a){
-        return 1;
-    }
-    else if(a>b){
-        return 2;
-    }
+    if(b>a)return 1;
+    else if(a>b)return 2;
       return 0;
   }
 }
