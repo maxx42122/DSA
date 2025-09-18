8be89c3e8ec877302db24e83c228e97d63891a8d
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-09-18 09:17:35 +0530

    09-18-2025 0125-valid-palindrome Easy .java 14 ms (31.06%) 45.1 MB (29.24%)

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,18 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // String p= s.toLowerCase().replaceAll("[^a-z0-9]", "").trim();
-          
-         char arr[]=s.toLowerCase().replaceAll("[^a-z0-9]", "").trim().toCharArray();
-        int start=0;
-        int end=arr.length-1;
-         while(start<end){
-             if(arr[start]!=arr[end]){
-                return false;
-             }
-             start++;
-             end--;
-         }
-         return true;
-         
+
+        char arr[]=s.toLowerCase().replaceAll("[^a-z0-9]","").trim().toCharArray();
+        int i=0;
+        int j=arr.length-1;
+        while(i<j){
+            if(arr[i]!=arr[j])return false;
+            i++;
+            j--;
+        }
+
+        return true;
     }
+       
 }
