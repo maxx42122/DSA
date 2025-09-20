87e5594efa2abc89574062717eda4f30aafde551
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-09-20 10:22:31 +0530

    09-20-2025 0643-maximum-average-subarray-i Easy .java 2 ms (99.92%) 56.7 MB (17.27%)

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -5,39 +5,13 @@ class Solution {
             sum+=arr[i];
         }
         int max=sum;
-          System.out.print(sum+" ");
         int x=0;
         for(int i=k;i<arr.length;i++){
             sum+=arr[i];
             sum-=arr[x++];
             if(sum>max)max=sum;
-              System.out.print(sum+" ");
 
         }
-
-        // int nums[]=new int[arr.length];
-
-        // int st=0;
-        // nums[0]=arr[0];
-     
-        // for(int i=1;i<arr.length;i++){
-        //     nums[i]=arr[i-1]+arr[i];
-        //      System.out.print(nums[i]+" ");
-        //        System.out.println();
-        
-        // }
-        // int sum=nums[0];
-        // int max=nums[0];
-
-        // for(int i=k;i<arr.length;i++){
-        //     sum=nums[k]-nums[st++];
-        //     if(sum>max){
-        //         max=sum;
-        //     }
-        //      System.out.print(sum+" ");
-        // }
-
-        
         return (double)max/k;
     }
 }
