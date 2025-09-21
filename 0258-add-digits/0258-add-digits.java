4a108e721006580e4f181a982b2a64347b8e202f
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-09-21 10:41:22 +0530

    09-21-2025 0258-add-digits Easy .java 1 ms (43.57%) 40.8 MB (72.03%)

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,23 +1,38 @@
 class Solution {
-    public int addDigits(int num) {
+  int s=0;
+ public int addDigits(int num) {
+    if(num==0)return 0;
 
+    s+=num%10;
+    
+    addDigits(num/10);
+     if(s>=10){
+        num=s;
+        s=0;
+        addDigits(num);
 
-        while(num>9){
-        num= checknum(num);
-        }
-        return num;
     }
-  
-   
-      int checknum(int num){
-           int sum=0;
-            while(num!=0){
-            sum+=num%10;
-        
 
-            num/=10;
-          
-            }
-            return sum;
-        }
+   
+
+    return s;
+ }
 }
+
+
+
+  
+
+
+    //     while (num >= 10) {
+    //         int sum = 0;
+    //         while (num > 0) {
+    //             sum += num % 10;  // Add last digit
+    //             num /= 10;        // Remove last digit
+    //         }
+    //         num = sum;
+    //     }
+    //     return num;
+    // }
+
+   
