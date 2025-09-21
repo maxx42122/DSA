2e27a5ec8c0622b1ff7dc27449673dac60b627db
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-09-21 10:43:07 +0530

    09-21-2025 0258-add-digits Easy .java 0 ms (100%) 40.7 MB (85.98%)

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,17 +1,31 @@
+// class Solution {
+//   int s=0;
+//  public int addDigits(int num) {
+//     if(num==0)return 0;
+//     s+=num%10;
+//     addDigits(num/10);
+//      if(s>=10){
+//         num=s;
+//         s=0;
+//         addDigits(num);
+//     }
+//     return s;
+//  }
+// }
 class Solution {
-  int s=0;
- public int addDigits(int num) {
-    if(num==0)return 0;
-    s+=num%10;
-    addDigits(num/10);
-     if(s>=10){
-        num=s;
-        s=0;
-        addDigits(num);
+    public int addDigits(int num) {
+        if (num < 10) return num;
+
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        return addDigits(sum);
     }
-    return s;
- }
 }
+
 
 
 
