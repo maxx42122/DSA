a4252e3ce675c3c97affb67cde7a9a754f1b0863
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-09-28 19:35:53 +0530

    09-28-2025 0009-palindrome-number Easy .java 5 ms (64.94%) 44.2 MB (47.98%)

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,17 +1,24 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int n=x;
-        int m=0;
 
-        while(x>0){
-            int dig=x%10;
-  m=(10*m)+dig;
-              x/=10;
-
-        }
-        if(m==n){
+        if(x>=0 && x==rev(x)){
             return true;
+
         }
         return false;
+
+
+        
+    }
+
+    int rev(int x){
+        int num=0;
+        while(x>0){
+            int dig=x%10;
+            num=(num*10)+dig;
+            x/=10;
+
+        }
+        return num;
     }
 }
