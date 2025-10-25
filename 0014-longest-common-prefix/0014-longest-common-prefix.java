7cff07bcd20d9bce4b28c2afee0f434bbb1cb6c8
Mayur Malhari Pawale <153881477+maxx42122@users.noreply.github.com>
2025-10-25 13:54:57 +0530

    10-25-2025 0014-longest-common-prefix Easy .java 0 ms (100%) 41.8 MB (41.6%)

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,20 +1,23 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        StringBuilder result=new StringBuilder();
-        Arrays.sort(strs);
+        if(strs.length==0 || strs==null)return "";
+        
+        String pref=strs[0];
+        // String s="lma";
+        // String t="lmx";
+        // System.out.println(s.indexOf(t));
 
-        char[] first=strs[0].toCharArray();
-          char[] last=strs[strs.length-1].toCharArray();
 
-          for(int i=0;i<first.length;i++){
-            if(first[i]==last[i]){
-                result.append(first[i]);
-            }else{
-                break;
+        for(int i=1;i<strs.length;i++){
+
+            while(strs[i].indexOf(pref)!=0){
+                pref=pref.substring(0,pref.length()-1);
+
+                if(pref.isEmpty())return "";
             }
-          }
+        }
+        return pref;
 
-          return result.toString();
+        
     }
 }
-        
